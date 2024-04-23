18a18e954f551a3c4c063076b27a5a010bcba98d
BENONI ESCKINDER <you@example.com>
2024-04-23 09:47:20 +0300

    Task 2

--- markdown2html.py
+++ markdown2html.py
@@ -23,21 +23,30 @@ if __name__ == "__main__":
 
     markdown_lines = parse_markdown(args[1])
     html_content = ""
+    list = []
     for line in markdown_lines:
         if line.startswith("# "):
-            html_content += f"<h1>{line[2:-1]}</h1>" + "\n"
+            html_content += f"<h1>{line[2:-1]}</h1>\n"
         elif line.startswith("## "):
-            html_content += f"<h2>{line[3:-1]}</h2>" + "\n"
+            html_content += f"<h2>{line[3:-1]}</h2>\n"
         elif line.startswith("### "):
-            html_content += f"<h3>{line[4:-1]}</h3>" + "\n"
+            html_content += f"<h3>{line[4:-1]}</h3>\n"
         elif line.startswith("#### "):
-            html_content += f"<h4>{line[5:-1]}</h4>" + "\n"
+            html_content += f"<h4>{line[5:-1]}</h4>\n"
         elif line.startswith("##### "):
-            html_content += f"<h5>{line[6:-1]}</h5>" + "\n"
+            html_content += f"<h5>{line[6:-1]}</h5>\n"
         elif line.startswith("###### "):
-            html_content += f"<h6>{line[7:-1]}</h6>" + "\n"
+            html_content += f"<h6>{line[7:-1]}</h6>\n"
+        elif line.startswith("- "):
+            list.append(line)
         else:
             html_content += f"<p>{line}</p>" + "\n"
+
+    if list:
+        html_content += "<ul>\n"
+        for element in list:
+            html_content += f"<li>{element[2:-1]}</li>\n"
+        html_content += "</ul>\n"
 
     # def write_to_html(html_content, html_file):
     with open(args[2], "w", encoding="utf-8") as f:
