7b956f61c6770cef164c7ee86cd912f3bee8860f
BENONI ESCKINDER <you@example.com>
2024-04-23 09:30:48 +0300

    Task 1

--- markdown2html.py
+++ markdown2html.py
@@ -6,13 +6,39 @@ import sys
 import os
 
 
-args = sys.argv
-if len(args) != 3:
-    print("Usage: ./markdown2html.py README.md README.html")
-    sys.exit(1)
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) != 3:
+        print("Usage: ./markdown2html.py README.md README.html")
+        sys.exit(1)
 
-if os.path.exists(args[1]):
-    sys.exit(0)
-else:
-    print(f"Missing {args[1]}")
-    sys.exit(1)
+    if not os.path.exists(args[1]):
+        print(f"Missing {args[1]}")
+        sys.exit(1)
+
+    def parse_markdown(markdown):
+        with open(markdown, "r", encoding="utf-8") as f:
+            markdown_lines = f.readlines()
+        return markdown_lines
+
+    markdown_lines = parse_markdown(args[1])
+    html_content = ""
+    for line in markdown_lines:
+        if line.startswith("# "):
+            html_content += f"<h1>{line[2:-1]}</h1>" + "\n"
+        elif line.startswith("## "):
+            html_content += f"<h2>{line[3:-1]}</h2>" + "\n"
+        elif line.startswith("### "):
+            html_content += f"<h3>{line[4:-1]}</h3>" + "\n"
+        elif line.startswith("#### "):
+            html_content += f"<h4>{line[5:-1]}</h4>" + "\n"
+        elif line.startswith("##### "):
+            html_content += f"<h5>{line[6:-1]}</h5>" + "\n"
+        elif line.startswith("###### "):
+            html_content += f"<h6>{line[7:-1]}</h6>" + "\n"
+        else:
+            html_content += f"<p>{line}</p>" + "\n"
+
+    # def write_to_html(html_content, html_file):
+    with open(args[2], "w", encoding="utf-8") as f:
+        f.write(html_content)
